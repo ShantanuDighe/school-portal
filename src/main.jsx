bd938669adfe04f89d10ee4f1004a1259136b2ef
ShantanuDighe <shantanudighe429@gmail.com>
2026-02-22 23:31:59 +0530

    Connected AppRoutes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css";
 
-<BrowserRouter>
-  <App />
-</BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);

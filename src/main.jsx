06dead52e69b1429b84b86b5d3df2e7c1a8fef6b
ShantanuDighe <shantanudighe429@gmail.com>
2026-02-22 23:26:41 +0530

    Updated App layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+<BrowserRouter>
   <App />
-)
+</BrowserRouter>

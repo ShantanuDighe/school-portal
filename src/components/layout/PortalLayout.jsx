8a2298aec174b1f386b2fad6546687ceb0064394
ShantanuDighe <shantanudighe429@gmail.com>
2026-02-23 11:51:05 +0530

    Fixed route syntax properly

--- src/components/layout/PortalLayout.jsx
+++ src/components/layout/PortalLayout.jsx
@@ -1,17 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "./Sidebar"
-import Topbar from "./Topbar"
 
 function PortalLayout({ role }) {
   return (
-    <div className="d-flex">
-      <Sidebar role={role} />
-      <div className="flex-grow-1">
-        <Topbar role={role} />
-        <div className="container-fluid p-4 bg-light min-vh-100">
-          <Outlet />
-        </div>
-      </div>
+    <div>
+      <h2>{role} Portal</h2>
+      <Outlet />
     </div>
   );
 }
